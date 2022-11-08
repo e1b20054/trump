3859ab29154fad97c9d041f5b29e4c213efa2944
e1b20054 <e1b20054@oit.ac.jp>
2022-11-08 14:57:44 +0900

    リポジトリの初期化

---
+++ src/main/java/group9/trump/TrumpApplication.java
@@ -0,0 +1,13 @@
+package group9.trump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group9/trump/TrumpApplicationTests.java
@@ -0,0 +1,13 @@
+package group9.trump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
