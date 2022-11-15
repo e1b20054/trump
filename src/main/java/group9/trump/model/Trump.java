684df25b991dbcc518c05eb75a137d01340373d5
e1b20106 <e1b20106@st.oit.ac.jp>
2022-11-15 14:55:21 +0900

    modelの作成

---
+++ src/main/java/group9/trump/model/Trump.java
@@ -0,0 +1,27 @@
+package group9.trump.model;
+
+public class Trump {
+  int id;
+  int num;
+  String mark;
+
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public int getNum() {
+    return num;
+  }
+  public void setNum(int num) {
+    this.num = num;
+  }
+  public String getMark() {
+    return mark;
+  }
+  public void setMark(String mark) {
+    this.mark = mark;
+  }
+
+}
